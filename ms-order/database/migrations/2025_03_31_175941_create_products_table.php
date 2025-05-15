2e201969bee9e73f9b463ff8527e90ba0c2676ec
victorstefan28 <victorstefan28@gmail.com>
2025-05-15 23:59:35 +0300

    fix CI for catalog

--- ms-order/database/migrations/2025_03_31_175941_create_products_table.php
+++ ms-order/database/migrations/2025_03_31_175941_create_products_table.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id()->primary();
+            $table->id();
             $table->string('name');
             $table->string('price');
             $table->string('image_url')->nullable();
